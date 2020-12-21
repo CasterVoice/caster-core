393d7827ac1a517c56718cb4087b6996ef350cde
Timoses <timosesu@gmail.com>
2020-12-21 11:40:26 +0100

    Fix missing __init__.py module files

---
+++ castervoice/core/plugin/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+Plugins allow Caster to be extended or may even bring initial
+functionality to Caster where Caster on its own does not
+provide any functionality.
+
+"""
+from .plugin import Plugin
+from .plugin_manager import PluginManager
