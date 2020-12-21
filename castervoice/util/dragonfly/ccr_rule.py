d1a56ea7c5cd79cb7abbde7242dd3611f000fc59
Timoses <timosesu@gmail.com>
2020-12-21 11:40:09 +0100

    Allow CCR rule to be created from multiple rules

--- castervoice/util/dragonfly/ccr_rule.py
+++ castervoice/util/dragonfly/ccr_rule.py
@@ -14,11 +14,14 @@ class CCRRule(CompoundRule):
                 action.execute()
 
     @staticmethod
-    def create(from_rule, max_repetitions=50):
-        original_rule = RuleRef(rule=from_rule)
-        single_action = Alternative([original_rule])
+    def create(*from_rules, max_repetitions=50):
+        name = "CCR"
+        rules = []
+        for rule in from_rules:
+            rules.append(RuleRef(rule=rule))
+            name += "_" + rule.name
+        single_action = Alternative(rules)
         sequence = Repetition(single_action, min=1, max=max_repetitions,
                               name="sequence")
-        myname = from_rule.name + "_ccr"
 
-        return CCRRule(name=myname, spec=CCRRule.spec, extras=[sequence])
+        return CCRRule(name=name, spec=CCRRule.spec, extras=[sequence])
